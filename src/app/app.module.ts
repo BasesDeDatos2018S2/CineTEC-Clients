77d62b772bffcb9a69fd7d3c436899f5e7699832
Gustavo Segura <yo@gustavosegura.com>
2018-11-24 23:58:43 -0600

    [new]	view screening component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScreeningsComponent } from './components/screenings/screenings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreeningsComponent
   ],
   imports: [
     BrowserModule
